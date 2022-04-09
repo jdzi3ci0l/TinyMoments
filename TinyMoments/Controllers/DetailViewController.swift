5098f17f4798b3d6ff382908ba39494b27584c09
jdzi3ci0l <j.dzi3ci0l@gmail.com>
2022-04-09 18:25:08 +0200

    Make delete button turn to Done while editing

--- TinyMoments/Controllers/DetailViewController.swift
+++ TinyMoments/Controllers/DetailViewController.swift
@@ -40,12 +40,13 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
+        titleTextField.delegate = self
         
         let gradient = CAGradientLayer()
         gradient.frame = view.bounds
         gradient.colors = [#colorLiteral(red: 0.8518229127, green: 0.8488650322, blue: 0.99630481, alpha: 1).cgColor, #colorLiteral(red: 0.9804552197, green: 0.8359815478, blue: 0.9779635072, alpha: 1).cgColor, #colorLiteral(red: 0.9792258143, green: 0.6606447101, blue: 0.681139946, alpha: 1).cgColor, #colorLiteral(red: 0.7292762399, green: 0.6618252397, blue: 0.8362961411, alpha: 1).cgColor]
         view.layer.insertSublayer(gradient, at: 0)
-    
+        
         textView.textContainer.lineFragmentPadding = 0
         
         titleTextField.text = entry.title
@@ -65,35 +66,70 @@ class DetailViewController: UIViewController {
         }
     }
     
-    @IBAction func deleteButtonPressed(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Delete Entry?", message: "This entry will be permanently deleted.", preferredStyle: .alert)
-        
-        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
-            self.context.delete(self.entry)
-            self.saveData()
-            self.navigationController?.popViewController(animated: true)
+    @IBAction func rightUIBarButtonPressed(_ sender: UIBarButtonItem) {
+        if textView.isFirstResponder || titleTextField.isFirstResponder {
+            textView.endEditing(true)
+            titleTextField.endEditing(true)
+        } else {
+            let alert = UIAlertController(title: "Delete Entry?", message: "This entry will be permanently deleted.", preferredStyle: .alert)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+                self.context.delete(self.entry)
+                self.saveData()
+                self.navigationController?.popViewController(animated: true)
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            alert.addAction(deleteAction)
+            alert.addAction(cancelAction)
+            
+            present(alert, animated: true, completion: nil)
         }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        
-        alert.addAction(deleteAction)
-        alert.addAction(cancelAction)
-        
-        present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func textFieldDidChange(_ sender: UITextField) {
-        entry.title = titleTextField.text
-        saveData()
+    func updateRightBarButtonItem(isCurrentlyEditing: Bool) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: isCurrentlyEditing ? .done : .trash,
+            target: self,
+            action: #selector(rightUIBarButtonPressed)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = isCurrentlyEditing ? #colorLiteral(red: 0.486708045, green: 0.4391390383, blue: 0.5955944061, alpha: 1) : .red
     }
 }
 
-//MARK: - UITextViewDelegate
+//MARK: - UITextViewDelegate methods
 
 extension DetailViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         entry.text = textView.text
         saveData()
     }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        updateRightBarButtonItem(isCurrentlyEditing: true)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        updateRightBarButtonItem(isCurrentlyEditing: false)
+    }
 }
 
+
+//MARK: - UITextFieldDelegate methods
+
+extension DetailViewController: UITextFieldDelegate {
+    
+    @IBAction func textFieldDidChange(_ sender: UITextField) {
+        entry.title = titleTextField.text
+        saveData()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        updateRightBarButtonItem(isCurrentlyEditing: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateRightBarButtonItem(isCurrentlyEditing: false)
+    }
+}
